9984b26ca13e42d435b585c358a02071fe119336
ChenPriew <suriya0957329813@gmail.com>
2024-02-02 21:39:47 +0700

    submitted

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -5,15 +5,15 @@ import actors from '../../../assets/actors.json';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent{
+export class MainComponent {
   movies = movies;
   actors = actors;
   mainActors = new Array();
 
   ngOnInit() {
-    for (let index = 0; index < 12; index+=2) {
+    for (let index = 0; index < 12; index += 2) {
       this.mainActors.push(actors[index]);
     }
   }
